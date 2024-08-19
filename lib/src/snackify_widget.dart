c408ef15a3b2a05dde96202ca835ee28726f1f0f
Atanu Sabyasachi <jena.atanusabyasachi@gmail.com>
2024-08-19 23:06:05 +0530

    code changed

--- lib/src/snackify_widget.dart
+++ lib/src/snackify_widget.dart
@@ -1,100 +1,30 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 
-class SnackifyWidget extends StatefulWidget {
-  const SnackifyWidget({
-    super.key,
-    required this.message,
-    this.backgroundColor = Colors.black,
-    this.textColor = Colors.white,
-    this.duration = const Duration(seconds: 3),
-    this.animationDuration = const Duration(milliseconds: 300),
-    this.snackbarHeight = 50.0,
-    this.snackbarWidth = double.infinity,
-    this.animationCurve = Curves.easeInOut,
-  });
-
-  final Curve animationCurve;
-  final Duration animationDuration;
-  final Color backgroundColor;
-  final Duration duration;
-  final String message;
-  final double snackbarHeight;
-  final double snackbarWidth;
-  final Color textColor;
-
-  @override
-  _SnackifyWidgetState createState() => _SnackifyWidgetState();
-}
-
-class _SnackifyWidgetState extends State<SnackifyWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Offset> _offsetAnimation;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: widget.animationDuration,
-      vsync: this,
-    );
-
-    _offsetAnimation = Tween<Offset>(
-      begin: const Offset(0.0, 1.0),
-      end: Offset.zero,
-    ).animate(CurvedAnimation(
-      parent: _controller,
-      curve: widget.animationCurve,
-    ));
-
-    _controller.forward();
-    _hideSnackbar();
-  }
-
-  void _hideSnackbar() {
-    Future.delayed(widget.duration, () {
-      _controller.reverse().then((_) {
-        if (mounted) {
-          Navigator.of(context).pop();
-        }
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SlideTransition(
-      position: _offsetAnimation,
-      child: Material(
-        color: widget.backgroundColor,
-        child: Container(
-          height: widget.snackbarHeight,
-          width: widget.snackbarWidth,
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Center(
-            child: Text(
-              widget.message,
-              style: TextStyle(color: widget.textColor),
-            ),
+class Message {
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> customMessage(
+    BuildContext context, {
+    double? screenWidth,
+    required String message,
+    Color? backgroundColor,
+    Color? iconCloseColor,
+  }) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        //width: screenWidth,
+        showCloseIcon: true,
+        closeIconColor: iconCloseColor ?? Colors.black,
+        duration: const Duration(seconds: 5),
+        content: Center(
+          child: Text(
+            message,
+            style: Theme.of(context).textTheme.titleLarge,
           ),
         ),
+        backgroundColor: backgroundColor ?? Colors.green,
+        elevation: 10,
+        behavior: SnackBarBehavior.floating,
       ),
     );
   }
-}
-
-void showSnackify(BuildContext context, String message) {
-  Navigator.of(context).push(PageRouteBuilder(
-    opaque: false,
-    barrierDismissible: true,
-    pageBuilder: (context, _, __) => SnackifyWidget(message: message),
-  ));
 }
